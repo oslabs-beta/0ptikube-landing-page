6e32567a5159d22ac62fa220e5e4285d707b7f8d
NotAStuhmer <j.stuhmer@gmail.com>
2025-02-28 00:07:34 -0500

    Made changes to next-sitemap.config.js that resulted in sitemap.xml being generated for Google SEO

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://0ptikube.dev',
-  generateRobotsTxt: true,
-  sitemapSize: 5000,
-  exclude: ['/404'],
+  siteUrl: 'https://optikube.dev', // Your live domain
+  generateRobotsTxt: true, // Also generate a robots.txt
+  sitemapSize: 5000, // Ensures large sites don’t break sitemap
+  exclude: ['/404'], // Exclude unwanted pages
+  additionalPaths: async () => [
+    { loc: '/', lastmod: new Date().toISOString() }, // Explicitly include homepage
+  ],
 };
