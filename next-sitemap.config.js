0f0116b0dda5560212cc303d3ca681d964f6cab0
NotAStuhmer <j.stuhmer@gmail.com>
2025-02-28 00:09:39 -0500

    More modifications to next-sitemap.config.js for creation of sitemap.xml for Google SEO

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://optikube.dev', // Your live domain
+  siteUrl: 'https://0ptikube.dev', // Your live domain
   generateRobotsTxt: true, // Also generate a robots.txt
   sitemapSize: 5000, // Ensures large sites don’t break sitemap
   exclude: ['/404'], // Exclude unwanted pages
